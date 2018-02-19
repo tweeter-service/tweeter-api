2bccb7eea2d28102e5d9edc9cf3225be1e12d828
Toshiaki Maki <tmaki@pivotal.io>
2018-02-19 17:09:10 +0900

    Okta sample

--- src/main/java/com/example/TweeterApiApplication.java
+++ src/main/java/com/example/TweeterApiApplication.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class TweeterApiApplication {
@@ -32,6 +34,11 @@ public class TweeterApiApplication {
 					.access("#oauth2.hasScope('tweet.write')")
 					.antMatchers(HttpMethod.PATCH, "/v1/**")
 					.access("#oauth2.hasScope('tweet.write')");
+		}
+
+		@Override
+		public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+			resources.resourceId("api://default");
 		}
 	}
 }
--- src/main/java/com/example/TweeterController.java
+++ src/main/java/com/example/TweeterController.java
@@ -9,12 +9,10 @@ import java.util.UUID;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class TweeterController {
 	private final JdbcTemplate jdbcTemplate;
 
